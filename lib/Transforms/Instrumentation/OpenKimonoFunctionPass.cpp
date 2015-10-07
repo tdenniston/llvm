873ddbca549c6fb9e066d8f0241e3ead236764a5
Damon Doucet <ddoucet@mit.edu>
2015-10-07 03:45:34 +0000

    Remove whitespace at the ends of lines and remove values from load/store hooks

--- lib/Transforms/Instrumentation/OpenKimonoFunctionPass.cpp
+++ lib/Transforms/Instrumentation/OpenKimonoFunctionPass.cpp
@@ -22,7 +22,7 @@
 
 using namespace llvm;
 
-// see 
+// see
 // http://llvm.org/docs/ProgrammersManual.html#the-debug-macro-and-debug-option
 // on how to use debugging infrastructure in LLVM
 // also used by STATISTIC macro, so need to define this before using STATISTIC
@@ -47,8 +47,8 @@ struct OpenKimonoFunctionPass : public FunctionPass {
 private:
   // compute the index that's log of its size based on the type that Addr
   // points to; used to index into the array based on AccessType
-  int getMemoryAccessFuncIndex(Value *Addr, const DataLayout &DL); 
-  // initialize OK instrumentation functions for load and store 
+  int getMemoryAccessFuncIndex(Value *Addr, const DataLayout &DL);
+  // initialize OK instrumentation functions for load and store
   void initializeLoadStoreCallbacks(Module &M);
   // initialize OK instrumentation functions for function entry and exit
   void initializeFuncCallbacks(Module &M);
@@ -110,17 +110,17 @@ void OpenKimonoFunctionPass::initializeFuncCallbacks(Module &M) {
       M.getOrInsertFunction("__ok_func_exit", IRB.getVoidTy(), nullptr));
 }
 
-/** 
+/**
  * initialize the declaration of instrumentation functions
- * 
+ *
  * void __ok_before_load<k>(void *addr, int attr);
- * void __ok_after_load<k>(void *addr, int attr, T val);
- * void __ok_before_store<k>(void *addr, int attr, T val);
+ * void __ok_after_load<k>(void *addr, int attr);
+ * void __ok_before_store<k>(void *addr, int attr);
  * void __ok_after_store<k>(void *addr, int attr);
  *
  * where <k> = 1, 2, 4, 8, and T is the type of the value read / written.
  * including (u)int8_t, (u)int16_t, (u)int32_t, (u)int64_t, float, and double.
- * 
+ *
  * Presumably aligned / unaligned accesses are specified by the attr
  * XXX: do we want the val to be signed or unsigned?
  * XXX: is there a separate type for unsigned int in LLVM?
@@ -139,44 +139,44 @@ void OpenKimonoFunctionPass::initializeLoadStoreCallbacks(Module &M) {
     const size_t ByteSize = 1 << i;
     const size_t BitSize = ByteSize * 8;
     Type *ValType = IRB.getIntNTy(BitSize);     // uint<k>_t val
-    
+
     AccessType[i] = ValType;
     AccessPtrType[i] = ValType->getPointerTo();
 
     // NOTE: nullptr is a new C++11 construct; denote a null pointer
-    // here, just used to denote end of args; 
+    // here, just used to denote end of args;
     // SmallString<32> long enough to hold 32 characters including '\0'.
-    
+
     // void __ok_before_load<k>(void *addr, int attr);
     SmallString<32> BeforeReadName("__ok_before_load" + itostr(ByteSize));
     OkBeforeRead[i] = checkOkInterfaceFunction(
-        M.getOrInsertFunction(BeforeReadName, RetType, 
-                              AddrType, AttrType, nullptr)); 
+        M.getOrInsertFunction(BeforeReadName, RetType,
+                              AddrType, AttrType, nullptr));
 
     // void __ok_after_load<k>(void *addr, int attr, T val);
     SmallString<32> AfterReadName("__ok_after_load" + itostr(ByteSize));
     OkAfterRead[i] = checkOkInterfaceFunction(
         M.getOrInsertFunction(AfterReadName, RetType,
-                              AddrType, AttrType, ValType, nullptr)); 
+                              AddrType, AttrType, nullptr));
 
     // void __ok_before_store<k>(void *addr, int attr, T val);
     SmallString<32> BeforeWriteName("__ok_before_store" + itostr(ByteSize));
     OkBeforeWrite[i] = checkSanitizerInterfaceFunction(
-        M.getOrInsertFunction(BeforeWriteName, RetType, 
-                              AddrType, AttrType, ValType, nullptr));
+        M.getOrInsertFunction(BeforeWriteName, RetType,
+                              AddrType, AttrType, nullptr));
 
     // void __ok_after_store<k>(void *addr, int attr);
     SmallString<32> AfterWriteName("__ok_after_store" + itostr(ByteSize));
     OkAfterWrite[i] = checkOkInterfaceFunction(
-        M.getOrInsertFunction(AfterWriteName, RetType, 
-                              AddrType, AttrType, nullptr)); 
+        M.getOrInsertFunction(AfterWriteName, RetType,
+                              AddrType, AttrType, nullptr));
   }
 
   // generate the type-distinct functions for float and double
-  // void __ok_after_load4(void *addr, int attr, float val);
-  // void __ok_after_load8(void *addr, int attr, double val);
-  // void __ok_before_store4(void *addr, int attr, float val);
-  // void __ok_before_store8(void *addr, int attr, float val);
+  // void __ok_after_load4(void *addr, int attr);
+  // void __ok_after_load8(void *addr, int attr);
+  // void __ok_before_store4(void *addr, int attr);
+  // void __ok_before_store8(void *addr, int attr);
   /*
   Type *FloatValType = IRB.getFloatTy();
   Type *DoubleValType = IRB.getDoubleTy();
@@ -184,22 +184,22 @@ void OpenKimonoFunctionPass::initializeLoadStoreCallbacks(Module &M) {
   SmallString<32> AfterReadFloat("__ok_after_load_float");
   OkAfterReadFloat = checkOkInterfaceFunction(
       M.getOrInsertFunction(AfterReadFloat, RetType,
-        AddrType, AttrType, FloatValType, nullptr)); 
+        AddrType, AttrType, nullptr));
 
   SmallString<32> AfterReadDouble("__ok_after_load_double");
   OkAfterReadDouble  = checkOkInterfaceFunction(
       M.getOrInsertFunction(AfterReadDouble, RetType,
-        AddrType, AttrType, DoubleValType, nullptr)); 
+        AddrType, AttrType, nullptr));
 
   SmallString<32> BeforeWriteFloat("__ok_before_store_float");
   OkBeforeWriteFloat = checkSanitizerInterfaceFunction(
-      M.getOrInsertFunction(BeforeWriteFloat, RetType, 
-        AddrType, AttrType, FloatValType, nullptr));
+      M.getOrInsertFunction(BeforeWriteFloat, RetType,
+        AddrType, AttrType, nullptr));
 
   SmallString<32> BeforeWriteDouble("__ok_before_store_double");
   OkBeforeWriteDouble = checkSanitizerInterfaceFunction(
-      M.getOrInsertFunction(BeforeWriteDouble, RetType, 
-        AddrType, AttrType, DoubleValType, nullptr));
+      M.getOrInsertFunction(BeforeWriteDouble, RetType,
+        AddrType, AttrType, nullptr));
   */
 }
 
@@ -210,7 +210,7 @@ int OpenKimonoFunctionPass::getMemoryAccessFuncIndex(Value *Addr,
   assert(OrigTy->isSized());
   uint32_t TypeSize = DL.getTypeStoreSizeInBits(OrigTy);
   if (TypeSize != 8  && TypeSize != 16 && TypeSize != 32 && TypeSize != 64) {
-    DEBUG_WITH_TYPE("ok-func", 
+    DEBUG_WITH_TYPE("ok-func",
         errs() << "Bad size " << TypeSize << " at addr " << Addr << "\n");
     NumAccessesWithBadSize++;
     return -1;
@@ -223,7 +223,7 @@ int OpenKimonoFunctionPass::getMemoryAccessFuncIndex(Value *Addr,
 bool OpenKimonoFunctionPass::instrumentLoadOrStore(inst_iterator Iter,
                                                    const DataLayout &DL) {
 
-  DEBUG_WITH_TYPE("ok-func", 
+  DEBUG_WITH_TYPE("ok-func",
       errs() << "OK_func: instrument instruction " << *Iter << "\n");
 
   Instruction *I = &(*Iter);
@@ -239,7 +239,7 @@ bool OpenKimonoFunctionPass::instrumentLoadOrStore(inst_iterator Iter,
   if (Idx < 0) return false; // size that we don't recognize
 
   /* XXX: This deals ww/ Alignment; come back later
-  const unsigned Alignment = IsWrite ? 
+  const unsigned Alignment = IsWrite ?
       cast<StoreInst>(I)->getAlignment() : cast<LoadInst>(I)->getAlignment();
 
   Type *OrigTy = cast<PointerType>(Addr->getType())->getElementType();
@@ -255,26 +255,24 @@ bool OpenKimonoFunctionPass::instrumentLoadOrStore(inst_iterator Iter,
     Function *BeforeF = OkBeforeWrite[Idx];
 
     /*
-    if(SType == IRB.getFloatTy()) { BeforeF = OkBeforeWriteFloat; } 
+    if(SType == IRB.getFloatTy()) { BeforeF = OkBeforeWriteFloat; }
     else if(SType == IRB.getDoubleTy()) { BeforeF = OkBeforeWriteDouble; }
     */
 
-    DEBUG_WITH_TYPE("ok-func", 
-        errs() << "OK_func: creating call to before store for size " 
+    DEBUG_WITH_TYPE("ok-func",
+        errs() << "OK_func: creating call to before store for size "
                << Idx << " and type " << SType << "\n");
     IRB.CreateCall(BeforeF,
         // XXX: should I just use the pointer type with the right size?
         {IRB.CreatePointerCast(Addr, AddrType),
-         IRB.getInt32(0), // XXX: use 0 for attr for now; FIXME 
-         // IRB.CreateIntCast(S->getValueOperand(), AccessType[Idx])});
-         S->getValueOperand()});
-    // move pass the actual store instruction; 
+         IRB.getInt32(0)}); // XXX: use 0 for attr for now; FIXME
+    // move pass the actual store instruction;
     // the inserted instruction doesn't seem to count.
     Iter++;
     IRB.SetInsertPoint(&*Iter);
 
-    DEBUG_WITH_TYPE("ok-func", 
-        errs() << "OK_func: creating call to after store for size " 
+    DEBUG_WITH_TYPE("ok-func",
+        errs() << "OK_func: creating call to after store for size "
                << Idx << "\n");
     IRB.CreateCall(OkAfterWrite[Idx],
         // XXX: should I just use the pointer type with the right size?
@@ -288,33 +286,29 @@ bool OpenKimonoFunctionPass::instrumentLoadOrStore(inst_iterator Iter,
     Function *AfterF = OkAfterRead[Idx];
 
     /*
-    if(LType == IRB.getFloatTy()) { AfterF = OkAfterReadFloat; } 
+    if(LType == IRB.getFloatTy()) { AfterF = OkAfterReadFloat; }
     else if(LType == IRB.getDoubleTy()) { AfterF = OkAfterReadDouble; }
     */
 
-    DEBUG_WITH_TYPE("ok-func", 
-        errs() << "OK_func: creating call to before load for size " 
+    DEBUG_WITH_TYPE("ok-func",
+        errs() << "OK_func: creating call to before load for size "
                << Idx << " and type " << LType << "\n");
     IRB.CreateCall(OkBeforeRead[Idx],
         // XXX: should I just use the pointer type with the right size?
         {IRB.CreatePointerCast(Addr, AddrType),
          IRB.getInt32(0)});
-    // move pass the actual load instruction; 
+    // move pass the actual load instruction;
     // the inserted instruction doesn't seem to count.
     Iter++;
     IRB.SetInsertPoint(&*Iter);
 
-    DEBUG_WITH_TYPE("ok-func", 
-        errs() << "OK_func: creating call to after load for size " 
+    DEBUG_WITH_TYPE("ok-func",
+        errs() << "OK_func: creating call to after load for size "
                << Idx << "\n");
     IRB.CreateCall(AfterF,
         // XXX: should I just use the pointer type with the right size?
         {IRB.CreatePointerCast(Addr, AddrType),
-         IRB.getInt32(0),
-         // In LLVM, the instruction pointer represents its result
-         // so we are basically passing the result of the load as 3rd arg
-         // IRB.CreateIntCast(L, AccessType[Idx])});
-         L});
+         IRB.getInt32(0)});
     NumInstrumentedReads++;
   }
 
@@ -325,15 +319,15 @@ bool OpenKimonoFunctionPass::doInitialization(Module &M) {
   DEBUG_WITH_TYPE("ok-func", errs() << "OK_func: doInitialization" << "\n");
   initializeFuncCallbacks(M);
   initializeLoadStoreCallbacks(M);
-  DEBUG_WITH_TYPE("ok-func", 
+  DEBUG_WITH_TYPE("ok-func",
       errs() << "OK_func: doInitialization done" << "\n");
-  
+
   return true;
 }
 
 bool OpenKimonoFunctionPass::runOnFunction(Function &F) {
 
-  DEBUG_WITH_TYPE("ok-func", 
+  DEBUG_WITH_TYPE("ok-func",
                   errs() << "OK_func: run on function " << F.getName() << "\n");
 
   SmallVector<Instruction*, 8> AllLoadsAndStores;
@@ -342,7 +336,7 @@ bool OpenKimonoFunctionPass::runOnFunction(Function &F) {
   bool Modified = false, HasCalls = false;
   const DataLayout &DL = F.getParent()->getDataLayout();
 
-  // Traverse all instructions in a function and insert instrumentation 
+  // Traverse all instructions in a function and insert instrumentation
   // on load & store
   for (inst_iterator I = inst_begin(F); I != inst_end(F); ++I) {
     // We need the Instruction Iterator to modify the code
@@ -379,7 +373,7 @@ bool OpenKimonoFunctionPass::runOnFunction(Function &F) {
   }
 
   if(Modified) {
-    DEBUG_WITH_TYPE("ok-func", 
+    DEBUG_WITH_TYPE("ok-func",
         errs() << "OK_func: modified function " << F.getName() << "\n");
   }
   return Modified;
